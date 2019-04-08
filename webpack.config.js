2eac8186002c4fea4c4ff4819a80c6b94e7caee5
s.takaaki <s.takaaki@baqemono.jp>
2019-04-08 14:46:12 +0900

    update

---
+++ webpack.config.js
@@ -0,0 +1,4 @@
+const sassBuildConfig = require('./_config/webpack.sass.config');
+const jsBuildConfig = require('./_config/webpack.javascript.config');
+
+module.exports = [sassBuildConfig, jsBuildConfig];
